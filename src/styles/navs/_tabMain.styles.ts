846e658c910b4ef7d6abda2efe8deda57be08bd5
Alvaro Cardozo <alvaro_car6@hotmail.com>
2022-08-17 20:43:12 -0500

    refactor: dot in navigator

--- src/styles/navs/_tabMain.styles.ts
+++ src/styles/navs/_tabMain.styles.ts
@@ -1,5 +1,6 @@
 import { colors, footer, padding } from "@src/styles/_global.styles";
 import { StyleSheet } from "react-native";
+import { color } from "react-native-reanimated";
 
 export const _tabMain = StyleSheet.create({
   container: {
@@ -14,7 +15,7 @@ export const _screenTab = StyleSheet.flatten({
     size: 35,
   },
   labelScreen: {
-    color: colors.white,
+    color: colors.secondary,
     size: 15,
   },
   screenSelected: {
