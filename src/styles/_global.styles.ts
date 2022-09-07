c446f00785e38787df748ac4800ffba069add7d7
Alvarocar <69275818+Alvarocar@users.noreply.github.com>
2022-09-07 11:41:58 -0500

    refactor: material ui (#1)

--- src/styles/_global.styles.ts
+++ src/styles/_global.styles.ts
@@ -6,7 +6,7 @@ export const dimensions = {
   fullWidth: Dimensions.get('window').width
 }
   
-export const colors  = {
+export const _colors  = {
   primary: '#E0B678',
   secondary: '#176368',
   tertiary: '#991E69',
@@ -18,32 +18,34 @@ export const colors  = {
 
 }
 
-export const padding = {
+export const _padding = {
   sm: 10,
   md: 20,
   lg: 30,
   xl: 40
 }
 
-export const fonts = {
+export const _fonts = {
   sm: 12,
   md: 18,
   lg: 28,
   primary: 'Roboto'
 }
 
-export const borderRadius = {
+export const _borderRadius = {
   sm: 10,
   md: 20,
   lg: 50,
 }
 
-export const footer = {
+export const _footer = {
   height: 60,
 }
 
-export const screenContainer: React.CSSProperties = {
-  backgroundColor: colors.secondary,
+export const _screenContainer = {
+  backgroundColor: _colors.secondary,
   width: dimensions.fullWidth,
   height: dimensions.fullHeight,
+  padding: _padding.sm,
+  paddingBottom: _padding.xl,
 }
